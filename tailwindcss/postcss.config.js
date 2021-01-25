f3802d9e0e5fa107e38724b89c11305d385d4d42
Javier Sánchez Mendoza <code@javier1nc.com>
2021-01-25 04:14:15 +0000

    Initial commit

---
+++ tailwindcss/postcss.config.js
@@ -0,0 +1,24 @@
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    purgecss({
+      content: [
+        //'./**/*.html',
+        '../*.html'
+        //Para agregar soporte para otro tipo de archivos.
+        // './**/*.js',
+        // './**/*.vue'
+      ],
+      //IMPORTANTE: Para soportar pseudo-clases
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    }),
+    require('cssnano')({
+      preset: 'default',
+    })
+  ]
+}
+
